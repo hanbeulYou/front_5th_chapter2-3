4faa1f598832cc2a9aec4c0599df416e89cc5810
hanbeulYou <hanbeul.you@gmail.com>
2025-04-27 21:48:58 +0900

    refactor: FetchTags 로직 분리

---
+++ src/entities/post/api/fetchTags.ts
@@ -0,0 +1,15 @@
+import { Tag } from "../../../pages/PostsManagerPage"
+
+// 태그 가져오기
+interface FetchTags {
+  setTags: React.Dispatch<React.SetStateAction<Tag[]>>
+}
+export const fetchTags = async ({ setTags }: FetchTags) => {
+  try {
+    const response = await fetch("/api/posts/tags")
+    const data = await response.json()
+    setTags(data)
+  } catch (error) {
+    console.error("태그 가져오기 오류:", error)
+  }
+}
--- src/entities/post/api/index.ts
+++ src/entities/post/api/index.ts
@@ -1,1 +1,2 @@
 export * from "./fetchPost"
+export * from "./fetchTags"
