fa12772f042b4020f12a1ed630477439cdc76b5e
hanbeulYou <hanbeul.you@gmail.com>
2025-04-28 01:41:31 +0900

    refactor: FetchUser 로직에 Response 타입 추가

--- src/entities/user/api/fetchUser.ts
+++ src/entities/user/api/fetchUser.ts
@@ -1,8 +1,12 @@
-interface FetchUser {
+import { User } from "../../../pages/PostsManagerPage"
+
+interface FetchUserRequest {
   userId: number
 }
 
-export const fetchUser = async ({ userId }: FetchUser) => {
+type FetchUserResponse = User
+
+export const fetchUser = async ({ userId }: FetchUserRequest): Promise<FetchUserResponse> => {
   const response = await fetch(`/api/users/${userId}`)
   const data = await response.json()
   return data
