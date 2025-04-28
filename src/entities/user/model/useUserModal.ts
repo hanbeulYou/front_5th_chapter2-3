4f2270cb4a529c058c4dfd788275766fbd89a78f
hanbeulYou <hanbeul.you@gmail.com>
2025-04-28 15:27:36 +0900

    fix: userModal에서 loading 상태관리 제거

--- src/entities/user/model/useUserModal.ts
+++ src/entities/user/model/useUserModal.ts
@@ -1,23 +1,18 @@
 import { useState } from "react"
 import { User } from "../model"
 import { fetchUser } from "../api/fetchUser"
-import { useLoadingStore } from "../../../shared/model"
 
 export const useUserModal = () => {
   const [selectedUser, setSelectedUser] = useState<User | null>(null)
   const [showUserModal, setShowUserModal] = useState(false)
-  const { setLoading } = useLoadingStore()
 
   const openUser = async (user: User) => {
-    setLoading(true)
     try {
       const userData = await fetchUser({ userId: user.id })
       setSelectedUser(userData)
       setShowUserModal(true)
-    } catch (e) {
-      console.error("사용자 정보 조회 실패", e)
-    } finally {
-      setLoading(false)
+    } catch (error) {
+      console.error("사용자 정보 가져오기 오류:", error)
     }
   }
 
