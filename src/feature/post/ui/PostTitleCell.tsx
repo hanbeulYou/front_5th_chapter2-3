3c945f20c1e582fa8284d6d24c9be745b98b0d83
hanbeulYou <hanbeul.you@gmail.com>
2025-04-29 00:43:24 +0900

    refactor: PostTitleCell UI 분리

---
+++ src/feature/post/ui/PostTitleCell.tsx
@@ -0,0 +1,19 @@
+import { Post } from "../../../entities/post/model"
+import { HighlightText } from "../../../shared/ui"
+import { TagItem } from "../../../feature/tag/ui"
+import { usePostFilterStore } from "../model"
+
+interface PostTitleCellProps {
+  post: Post
+}
+
+export const PostTitleCell = ({ post }: PostTitleCellProps) => {
+  const { searchQuery } = usePostFilterStore()
+  return (
+    <div className="space-y-1">
+      <div>{HighlightText(post.title, searchQuery)}</div>
+
+      <div className="flex flex-wrap gap-1">{post.tags?.map((tag) => <TagItem key={tag} tag={tag} />)}</div>
+    </div>
+  )
+}
