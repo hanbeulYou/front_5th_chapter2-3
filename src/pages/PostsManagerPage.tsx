80d98efe6fd28482cccb8de10694a420676de1f8
hanbeulYou <hanbeul.you@gmail.com>
2025-05-01 05:23:58 +0900

    refactor: PostAddButton 컴포넌트 분리

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -1,51 +1,29 @@
-import { Plus } from "lucide-react"
-import { Button, Card, CardContent, CardHeader, CardTitle } from "../shared/ui"
+import { Card, CardContent, CardHeader, CardTitle } from "../shared/ui"
 import { useUserModal } from "../entities/user/model"
 import { UserModal } from "../entities/user/ui"
-import {
-  usePostFilterStore,
-  useDialogStore,
-  usePostsQuery,
-  useSearchStore,
-  // useDeletePostMutation,
-} from "../feature/post/model"
+import { usePostFilterStore, usePostsQuery, useSearchStore } from "../feature/post/model"
 import { PostTable } from "../widgets/post/ui"
-import { Pagination, PostAddDialog, PostDetailDialog, PostEditDialog } from "../feature/post/ui"
+import { Pagination, PostAddButton, PostAddDialog, PostDetailDialog, PostEditDialog } from "../feature/post/ui"
 import { CommentAddDialog, CommentEditDialog } from "../feature/comment/ui"
 import { Filter } from "../widgets/filter/ui"
 
 const PostsManager = () => {
   const { showUserModal, setShowUserModal, selectedUser } = useUserModal()
 
-  const { setShowAddDialog } = useDialogStore()
-
   const { skip, limit, selectedTag } = usePostFilterStore()
   const { searchQuery } = useSearchStore()
 
   const { data: postsData, isLoading: isPostsLoading } = usePostsQuery({ limit, skip, tag: selectedTag, searchQuery })
 
-  // const deletePostMutation = useDeletePostMutation()
-
   const posts = postsData?.posts || []
   const isLoading = isPostsLoading
-
-  // const handleDeletePost = async (id: number) => {
-  //   try {
-  //     await deletePostMutation.mutateAsync(id)
-  //   } catch (error) {
-  //     console.error("게시물 삭제 오류:", error)
-  //   }
-  // }
 
   return (
     <Card className="w-full max-w-6xl mx-auto">
       <CardHeader>
         <CardTitle className="flex items-center justify-between">
           <span>게시물 관리자</span>
-          <Button onClick={() => setShowAddDialog(true)}>
-            <Plus className="w-4 h-4 mr-2" />
-            게시물 추가
-          </Button>
+          <PostAddButton />
         </CardTitle>
       </CardHeader>
       <CardContent>
