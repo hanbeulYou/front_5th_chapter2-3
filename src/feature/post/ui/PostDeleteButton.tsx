402e09d83bb19ed5476080b49cadb8927c0282e4
hanbeulYou <hanbeul.you@gmail.com>
2025-05-01 05:24:14 +0900

    fix: PostDeleteButton 내 삭제 로직 수정

--- src/feature/post/ui/PostDeleteButton.tsx
+++ src/feature/post/ui/PostDeleteButton.tsx
@@ -7,10 +7,18 @@ interface PostDeleteButtonProps {
   post: Post
 }
 export const PostDeleteButton = ({ post }: PostDeleteButtonProps) => {
-  const deletePostById = useDeletePostMutation()
+  const deletePostMutation = useDeletePostMutation()
+
+  const handleDeletePost = async (id: number) => {
+    try {
+      await deletePostMutation.mutateAsync(id)
+    } catch (error) {
+      console.error("게시물 삭제 오류:", error)
+    }
+  }
 
   return (
-    <Button variant="ghost" size="sm" onClick={() => deletePostById.mutate(post.id)}>
+    <Button variant="ghost" size="sm" onClick={() => handleDeletePost(post.id)}>
       <Trash2 className="w-4 h-4" />
     </Button>
   )
