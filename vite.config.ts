9a7caf8ac88373063920c495e050456549353676
hanbeulYou <hanbeul.you@gmail.com>
2025-05-02 02:30:55 +0900

    chore: 배포 관련 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { resolve } from "node:path"
+
+export const BASE_PATH = process.env.NODE_ENV === "production" ? "/front_5th_chapter2-3/" : "/"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: BASE_PATH,
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        origin: resolve(__dirname, "index.origin.html"),
+        refactoring: resolve(__dirname, "index.refactoring.html"),
+      },
+    },
+  },
+
   server: {
     proxy: {
       "/api": {
