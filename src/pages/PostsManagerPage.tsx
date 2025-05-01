c343001806d02185cc619ce33047d5d4dd488796
hanbeulYou <hanbeul.you@gmail.com>
2025-05-02 00:28:56 +0900

    fix: URL searchParams 접근시 setFilter 및 setSearchQuery 로직 동작하도록 수정

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -1,7 +1,12 @@
 import { Card } from "../shared/ui"
 import { PostAdminHeader, PostAdminBody, Dialogs } from "../widgets"
+import { usePostQueryParams } from "../feature/post/model"
 
 const PostsManager = () => {
+  // 이거 진짜 아닌거 같은데, 이거 빼면 url로 쿼리 접근시 받아올 수가 없습니다.
+  // 이거 때문에 해당 page 구성을 feature로 내리거나(widget으로도 어차피 못내린다고 생각), 각 feature에 이 훅을 부르는건 더 아닌거같아서 눈물을 머금고 여기에 적었습니다.
+  usePostQueryParams()
+
   return (
     <Card className="w-full max-w-6xl mx-auto">
       <PostAdminHeader />
